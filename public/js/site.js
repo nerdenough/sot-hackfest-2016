0a877c3a26ea9636d7df67dfcf49f7c712408bf0
Brendan Goodenough <brendan@goodenough.nz>
2016-08-21 09:30:50 +1200

    Load json data

--- public/js/site.js
+++ public/js/site.js
@@ -2,9 +2,16 @@ var sliderConfig = {
   change: updateMap
 };
 
+function loadData () {
+  $.getJSON('/data_json/2016_June.json', function (data) {
+    console.log(data);
+  });
+}
+
 function updateMap (event, ui) {
   var value = ui.value;
   console.log(value);
 }
 
 $('#slider').slider(sliderConfig);
+loadData();
