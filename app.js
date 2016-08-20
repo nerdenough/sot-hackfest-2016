d8f9537d73b3b4316fac4de3e2903cf8258ee115
Zachariah Macfarlane <zachmacfarlane@live.com>
2016-08-20 14:14:41 +1200

    slightly less broken skelton

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+// var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
-var clientSessions = require("client-sessions");
+// var clientSessions = require("client-sessions");
 
 var routes = require('./routes/index');
 
@@ -13,12 +12,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(clientSessions({
-  secret: 'WeDontCareAboutEncryption' // CHANGE THIS IRL!
-}));
+// app.use(clientSessions({
+//   secret: 'WeDontCareAboutEncryption' // CHANGE THIS IRL!
+// }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
