0b213600534bd4ebeb77878805e6f428c33bdfe5
Zachariah Macfarlane <zachmacfarlane@live.com>
2016-08-20 14:39:06 +1200

    updated package.json file

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router  = express.Router();
 
 router.get('/', function(req, res) {
-  res.sendFile('index.html');
+  res.sendFile('../views/index.html');
+  res.sendStatus(200);
 })
 
 module.exports = router;
