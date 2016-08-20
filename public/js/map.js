9576d4bcf2752fb7723f4de2a40aced9c7db9bca
William Irwin-Harris <w.irwinharris@gmail.com>
2016-08-21 11:35:31 +1200

    Implement initial version of map coloring

--- public/js/map.js
+++ public/js/map.js
@@ -8,67 +8,94 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     id: 'mapbox.light'
 }).addTo(map);
 
-$.getJSON("/map/NZDHB.json", function(data){ geojson = L.geoJson(data,{
-                style: style,
-                onEachFeature: onEachFeature} ).addTo(map);
-      });
+function createMap(diseaseIndex, year) {
+  var baseColorR = 255;
+  var baseColorG = 255;
+  var baseColorB = 255;
 
-		 
+  $.getJSON('/data_json/' + year + '_June.json', function (diseaseJSON) {
 
+    var maxCases = diseaseJSON[diseaseIndex].max;
+    var dhbIDToNumCasesMap = diseaseJSON[diseaseIndex].values;
 
-/*********
-FUNCTIONS
-*********/
+    $.getJSON("/map/NZDHB.json", function(data){ geojson = L.geoJson(data,{
+      style: style,
+      onEachFeature: onEachFeature} ).addTo(map);
+    });
 
-  
-//Style of map
-  function style() {
-      return {
-         
-          fillColor: '#fff',
+    /*********
+    FUNCTIONS
+    *********/
+
+    function computeColor(dhbID) {
+      var fraction = dhbIDToNumCasesMap[dhbID-1] / maxCases;
+      var r = Math.round(fraction * baseColorR);
+      var g = Math.round(fraction * baseColorG);
+      var b = Math.round(fraction * baseColorB);
+      return "rgb(" + r + ", " + g + ", " + b + ")";
+    }
+
+    //Style of map
+      function style() {
+          return {
+              fillColor: '#000',
+              weight: 1,
+              opacity: 1,
+              color: '#bdbdbd',
+              dashArray: '3',
+              fillOpacity: 0.7
+          };
+      }
+
+    function setupStyle(feature, layer, isHighlighted) {
+      if (isHighlighted) {
+        layer.setStyle({
+            weight: 2,
+            color: 'rgb(254, 166, 6)',
+            dashArray: '',
+            fillOpacity: 1.0,
+            fillColor:'rgb(254, 166, 6)'
+        });
+      } else {
+        layer.setStyle({
+          color: '#bdbdbd',
+          fillColor: computeColor(feature.id),
           weight: 1,
           opacity: 1,
-          color: '#bdbdbd',
           dashArray: '3',
           fillOpacity: 0.7
-      };
-  }
-
-//Style when mouse hovers
-  function highlightFeature(e) {
-    var layer = e.target;
-
-    layer.setStyle({
-        weight: 2,
-        color: '#fea606',
-        dashArray: '',
-        fillOpacity: 1.0,
-        fillColor:'#fea606'
-    });
-
-    if (!L.Browser.ie && !L.Browser.opera) {
-        layer.bringToFront();
+        });
+      }
     }
+
+    //Style when mouse hovers
+      function highlightFeature(e, feature) {
+
+        setupStyle(feature, e.target, true);
+
+        if (!L.Browser.ie && !L.Browser.opera) {
+            e.target.bringToFront();
+        }
+    }
+     //Reset styles when mouse out
+    function resetHighlight(e, feature) {
+        setupStyle(feature, e.target, false);
+    }
+
+    function zoomToFeature(e) {
+        map.fitBounds(e, e.target.getBounds());
+    }
+
+
+    function onEachFeature(feature, layer) {
+        setupStyle(feature, layer, false);
+        layer.on({
+            mouseover: function(e) { highlightFeature(e, feature) },
+            mouseout: function(e) { resetHighlight(e, feature) },
+            click: zoomToFeature
+        });
+    }
+  });
 }
- //Reset styles when mouse out
-function resetHighlight(e) {
 
-    geojson.resetStyle(e.target);
-}
-
-function zoomToFeature(e) {
-    map.fitBounds(e.target.getBounds());
-}
-
-
-function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: zoomToFeature
-    });
-}
-
-
-    
-	
+createMap(0, 2016);
