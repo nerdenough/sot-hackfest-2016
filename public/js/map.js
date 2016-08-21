413231cc85eb56ba24354db55eb29786c3c070c4
William Irwin-Harris <w.irwinharris@gmail.com>
2016-08-21 14:13:07 +1200

    Highlight info: fix off-by-one error with IDs

--- public/js/map.js
+++ public/js/map.js
@@ -74,7 +74,7 @@ function createMap(diseaseIndex, year) {
         /*TODO draw something*/
         console.log(feature.id);
         // $('#highlight-info').append("<p>" + dhbIDToNumCasesMap[feature.id] + "</p>");
-        $('#info').html("<p>" + dhbIDToNumCasesMap[feature.id] + "</p>");
+        $('#info').html("<p>" + dhbIDToNumCasesMap[feature.id-1] + "</p>");
         setupStyle(feature, e.target, true);
 
         if (!L.Browser.ie && !L.Browser.opera) {
